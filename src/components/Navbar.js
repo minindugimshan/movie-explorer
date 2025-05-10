35c1f7d824e0b352c589dcca728dcd6cb3b00d7d
minindugimshan2002 <minindugimshan11@gmail.com>
2025-05-10 23:54:13 +0530

    add favorites items to the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,3 +29,48 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// import React, { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+// import { AppBar, Toolbar, Typography, Button, Box, Switch } from '@mui/material';
+// import { ThemeContext } from '../context/ThemeContext';
+// import Brightness4Icon from '@mui/icons-material/Brightness4';
+// import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+// const Navbar = ({ isAuthenticated, onLogout }) => {
+//   const { darkMode, toggleTheme } = useContext(ThemeContext);
+
+//   return (
+//     <AppBar position="static">
+//       <Toolbar>
+//         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//           <Button color="inherit" component={Link} to="/">
+//             Movie Explorer
+//           </Button>
+//         </Typography>
+        
+//         {isAuthenticated ? (
+//           <>
+//             <Button color="inherit" component={Link} to="/favorites">
+//               Favorites
+//             </Button>
+//             <Button color="inherit" onClick={onLogout}>
+//               Logout
+//             </Button>
+//           </>
+//         ) : (
+//           <Button color="inherit" component={Link} to="/login">
+//             Login
+//           </Button>
+//         )}
+        
+//         <Box display="flex" alignItems="center" ml={2}>
+//           {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+//           <Switch checked={darkMode} onChange={toggleTheme} color="default" />
+//         </Box>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// };
+
+// export default Navbar;
